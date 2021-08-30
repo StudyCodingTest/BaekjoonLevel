6aee6bcdf87740e1c0b85ff6143ab7b7a25bb43a
wowo0709 <wjsduddn0709@naver.com>
2021-08-30 17:29:31 +0900

    upload baekjoon level backtracking 6 ~ DP1 7

---
+++ Level14_BackTracking/Step06/wowo0709.py
@@ -0,0 +1,30 @@
+# 스도쿠
+# 872ms(PyPy3)
+def solve_sudoku(cnt):
+    global sudoku, coo_mat, row, col, sqr
+    if cnt == len(coo_mat):
+        for row in sudoku: print(*row)
+        exit(0) # 한 가지 경우만 탐색
+
+    i,j = coo_mat[cnt]
+    for n in range(1,10):
+        if row[i][n] == col[j][n] == sqr[i//3*3+j//3][n] == 0:
+            row[i][n] = col[j][n] = sqr[i//3*3+j//3][n] = 1
+            sudoku[i][j] = n
+            solve_sudoku(cnt+1)
+            sudoku[i][j] = 0
+            row[i][n] = col[j][n] = sqr[i//3*3+j//3][n] = 0
+            
+
+sudoku, coo_mat = [], []
+row = [[0 for _ in range(10)] for _ in range(9)]
+col = [[0 for _ in range(10)] for _ in range(9)]
+sqr = [[0 for _ in range(10)] for _ in range(9)]
+for i in range(9): 
+    sudoku.append(list(map(int, input().split())))
+    for j in range(9):
+        n = sudoku[i][j]
+        if n == 0: coo_mat.append((i,j))
+        else: row[i][n] = col[j][n] = sqr[i//3*3+j//3][n] = 1
+
+solve_sudoku(0)
