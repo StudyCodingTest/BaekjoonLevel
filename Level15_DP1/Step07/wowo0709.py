90ca381b379da9831734211d72b52a0624f5d5ed
wowo0709 <wjsduddn0709@naver.com>
2021-09-02 13:12:58 +0900

    update baekjoon DP1 7

--- Level15_DP1/Step07/wowo0709.py
+++ Level15_DP1/Step07/wowo0709.py
@@ -1,11 +1,9 @@
 # 계단 오르기
-# 88ms
-n = int(input())
-# 계단 점수, 전계단 안 밟은 최대 점수, 전계단 밟은 최대 점수
-stairs = [[0,0,0]]+[[int(input()),0,0] for _ in range(n)]
-stairs[1][1] = stairs[1][2] = stairs[1][0]
-for i in range(2,n+1):
-    stairs[i][1] = stairs[i][0] + max(stairs[i-2][1:])
-    stairs[i][2] = stairs[i][0] + stairs[i-1][1]
-print(max(stairs[-1][1:]))
+# 76ms
+# 전계단 안 밟은 최대 점수, 전계단 밟은 최대 점수
+stairs = [[0,0]]+[[int(input())]*2 for _ in range(int(input()))]
+for i in range(2,len(stairs)):
+    stairs[i][0] += max(stairs[i-2])
+    stairs[i][1] += stairs[i-1][0]
+print(max(stairs[-1]))
 
