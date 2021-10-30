e5ea1994b6d3b1e15855f4f2fc5f62e0316c47c1
wowo0709 <wjsduddn0709@naver.com>
2021-10-30 22:16:48 +0900

    update divide&conquer 3,4

--- Level20_Divide&Conquer/Step03/wowo0709.py
+++ Level20_Divide&Conquer/Step03/wowo0709.py
@@ -1,2 +1,26 @@
 # 종이의 개수
-# 
+# 6072ms
+import sys
+N = int(input())
+papers = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+ans = [0,0,0]
+
+def count_papers(N,row,col):
+    global ans
+    first = papers[row][col]
+    for i in range(row,row+N):
+        for j in range(col,col+N):
+            if first != papers[i][j]: break
+        else: continue
+        break
+    else: # 모두 같은 종이일 때
+        ans[first+1] += 1
+        return
+    # 다른 종이가 있을 때
+    for i in range(3):
+        for j in range(3):
+            count_papers(N//3,row + N//3*i,col + N//3*j)
+    return
+
+count_papers(N,0,0)
+print(*ans,sep='\n')
